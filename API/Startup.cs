eee6f3b6d86e44e45ad7da04c28f1eef682a01f2
DJ <digambar.jalgaonkar@rhealtech.com>
2020-10-11 01:42:58 +0530

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,7 +27,7 @@ namespace API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        // 
+        // Ordering is not mater in this method
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -39,9 +39,11 @@ namespace API
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Ordering is very imp
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -54,6 +56,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
